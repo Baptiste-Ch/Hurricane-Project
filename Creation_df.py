0f6f6d92a3b83c30756dc585163038bc82af5a49
Baptiste-Ch <75433081+Baptiste-Ch@users.noreply.github.com>
2022-02-07 10:15:19 +0100

    Update Creation_df.py

--- Creation_df.py
+++ Creation_df.py
@@ -4,37 +4,43 @@
 
 import pandas as pd
 import numpy as np
+from numpy.random import randint
+from numpy.random import seed
 import netCDF4 as nc
 import datetime
+from netCDF4 import Dataset
+
+# IMPORT AND MERGE NETCDF FILES
+
+p_surf = nc.MFDataset(['E:/Data_hurrican/surface_pressure_1.nc', 
+                       'E:/Data_hurrican/surface_pressure_2.nc'], aggdim = 'time')
+sst = nc.MFDataset(['E:/Data_hurrican/surface_temperature_1.nc', 
+                    'E:/Data_hurrican/surface_temperature_2.nc'], aggdim = 'time')
+prcp = nc.MFDataset(['E:/Data_hurrican/prcp_1.nc', 
+                     'E:/Data_hurrican/prcp_2.nc'], aggdim = 'time')
+mslp = nc.MFDataset(['E:/Data_hurrican/mslevel_pressure_1.nc', 
+                       'E:/Data_hurrican/mslevel_pressure_2.nc'], aggdim = 'time')
+evap = nc.MFDataset(['E:/Data_hurrican/evaporation_1.nc', 
+                       'E:/Data_hurrican/evaporation_2.nc'], aggdim = 'time')
 
 
-ds2 = nc.Dataset('E:/Data_hurrican/surface_pressure_1.nc')                              # import des data Netcdf
+# ETABLISSEMENT DE L HEURE & COORDONNEES
 
-# print(ds2.variables.keys())
+time = pd.DataFrame(p_surf.variables['time'][:], columns = ['t'])
 
-# ETABLISSEMENT DE L HEURE
-time_psurf = pd.DataFrame(ds2.variables['time'][:], columns = ['t'])                    # je selectionne que le panel de dates qu on etudie et j en fais un df
-
-time = []
-for i in time_psurf.t:
+time_inter = []
+for i in time.t:
     x = datetime.datetime(1900,1,1,0) + datetime.timedelta(hours = i)
-    time.append(x)    
-time_psurf['time'] = time
-time_psurf = time_psurf.reset_index()                                                   # tout ca c est pour trouver la vraie date issue de la colonne de base qu on
+    time_inter.append(x)    
+time['time'] = time_inter
+time = time.reset_index()
 
 
+lat = pd.DataFrame(p_surf.variables['latitude'][:], columns = ['latitude'])
+lat = lat.reset_index()
 
-
-
-
-
-# ETABLISSEMENT COORDONNEES
-lat = pd.DataFrame(ds2.variables['latitude'][:], columns = ['latitude'])
-lat = lat.reset_index()                                                                 # les deux lignes pour definir le panel de latitudes qu on a et surtout definir quelle latitude se trouve en quel position du jeu Netcdf
-
-lon = pd.DataFrame(ds2.variables['longitude'][:], columns = ['longitude'])
-lon = lon.reset_index()                                                                 # pareil ici pour la longitude
-
+lon = pd.DataFrame(p_surf.variables['longitude'][:], columns = ['longitude'])
+lon = lon.reset_index()
 
 
 
@@ -42,18 +48,47 @@ lon = lon.reset_index()                                                         
 
 # EXTRACTION DES DONNEES
 
-df = pd.read_csv('IBTrACS.csv', sep = ';', na_values=' ')                               # on joue avec ibtracs mtn
+df = pd.read_csv('IBTrACS.csv', sep = ';', na_values=' ')
 df['date'] =  pd.to_datetime(df[['year', 'month', 'day', 'hour']])
 df2 = df.iloc[:,[5,7,8,9,10,11,24]] # selection des colonnes
-df2 = df2[df2['nature'] == 1]                                                           # tout ca c est pour selectionner que ce qui m interesse dans les donnees
+df2 = df2[df2['nature'] == 1]
 
-merge = time_psurf.merge(df2, left_on = 'time', right_on = 'date', how = 'inner')       # je fusionne ibtracs et les indexs de temps qu on a defini dans time_psurf (ligne 24)
-merge = merge.reset_index()                                                             
+merge = time.merge(df2, left_on = 'time', right_on = 'date', how = 'inner')
+merge = merge.reset_index()
 
-psurf = []                                                                              # je prepare un colonne vide pour mettre la pression de chaque ligne d Ibtracs
+
+inter_psurf = []
+inter_sst =   []
+inter_mslp =  []
+inter_evap =  []
+inter_prcp =  []
+full_list = [inter_psurf, inter_sst, inter_mslp, inter_evap, inter_prcp]
+full_header = ['psurf', 'sst', 'mslp', 'evaporation', 'prcp']
+
 for j in range(len(merge)):
-    y = lat[lat['index'] == lat['latitude'].sub(merge.latitude[j]).abs().idxmin()]      # On obtient la latitude du Netcdf la plus proche de la latitude issue de la ligne j de ibtracs (df merge)
-    z = lon[lon['index'] == lon['longitude'].sub(merge.longitude[j]).abs().idxmin()]    # pareil pour la longitude
-    x = ds2.variables['sp'][merge.iloc[j,1], y['index'], z['index']]                    # mtn je cherche la valeur de 's' correspondant a la lat/lon de la ligne j et au temps egal au temps de la ligne j
-    psurf.append(x)                                                                     # je met l info dans l array psurf et on laisse la boucle remplir l array
-merge['psurf'] = np.concatenate(psurf, axis = 0)                                        # petite astuce pour bien faire rentrer les valeurs de pression dans le dossier Ibtracs (merge)
+    y = lat[lat['index'] == lat['latitude'].sub(merge.latitude[j]).abs().idxmin()]
+    z = lon[lon['index'] == lon['longitude'].sub(merge.longitude[j]).abs().idxmin()]
+    
+    x_psurf = p_surf.variables['sp'][merge.iloc[j,1], y['index'], z['index']]
+    inter_psurf.append(x_psurf)
+    
+    x_sst = sst.variables['sst'][merge.iloc[j,1], y['index'], z['index']]
+    inter_sst.append(x_sst)
+    
+    x_mslp = mslp.variables['msl'][merge.iloc[j,1], y['index'], z['index']]
+    inter_mslp.append(x_mslp)
+    
+    x_evap = evap.variables['e'][merge.iloc[j,1], y['index'], z['index']]
+    inter_evap.append(x_evap)
+    
+    x_prcp = prcp.variables['tp'][merge.iloc[j,1], y['index'], z['index']]
+    inter_prcp.append(x_prcp)
+    
+    
+for i in range(len(full_list)):
+    merge['{}'.format(full_header[i])] = np.concatenate(full_list[i], axis = 0)
+
+
+
+
+# ADDITION OF RANDOM METEOROLOGICAL STATES
