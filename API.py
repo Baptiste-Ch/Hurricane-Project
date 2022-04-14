32c96d0208d71e177841b72fb5294c4446f16a56
Baptiste-Ch <75433081+Baptiste-Ch@users.noreply.github.com>
2022-04-14 14:02:19 +0200

    Update API.py

--- API.py
+++ API.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier #Réseau de forêt aléatoire
 import netCDF4 as nc
 import matplotlib.pyplot as plt
@@ -18,7 +17,8 @@ import cartopy
 import datetime as dt
 import os
 import imageio
-
+from sklearn.metrics import accuracy_score
+from Optimization import modelisation
 
    
 # API
@@ -26,7 +26,7 @@ import imageio
     # Mise sous forme de fonction pour être utilisable en boucle
 def API(year, month, day, hour, name):
     '''
-    format : 2020, 10, 20, 18:00, wilfred.nc
+    format exemple : 2020, 10, 20, 18:00, wilfred.nc
     '''
     
     c = cdsapi.Client()
@@ -39,11 +39,12 @@ def API(year, month, day, hour, name):
             'month': '{}'.format(month),
             'day': '{}'.format(day),
             'time': '{}:00'.format(hour),
-            'area': [ 40, -100, 7, 30],
+            'area': [40, -100, 7, 30],
             'variable': [
                 '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
                 '2m_temperature', 'evaporation', 'mean_sea_level_pressure',
-                'sea_surface_temperature', 'surface_pressure', 'total_precipitation' ],
+                'sea_surface_temperature', 'surface_pressure', 'total_precipitation',
+                '100m_u_component_of_wind', '100m_v_component_of_wind'],
             },
         '{}'.format(name))
     return nc.Dataset(name)
@@ -54,7 +55,7 @@ def to_df(ds):
     '''
     ds : name of the netcdf dataset
     '''
-    
+
     lon = pd.DataFrame(ds.variables['longitude'][:])
     lat = pd.DataFrame(ds.variables['latitude'][:])
     uwind = pd.DataFrame(ds.variables['u10'][0,:,:])
@@ -66,6 +67,9 @@ def to_df(ds):
     sst = pd.DataFrame(ds.variables['sst'][0,:,:])
     psurf = pd.DataFrame(ds.variables['sp'][0,:,:])
     prcp = pd.DataFrame(ds.variables['tp'][0,:,:])
+    u100 = pd.DataFrame(ds.variables['u100'][0,:,:])
+    v100 = pd.DataFrame(ds.variables['v100'][0,:,:])
+
 
     df_lat =np.array([lat,]*len(lon)).ravel()
     df_lon = np.array([lon,]*len(lat)).transpose().ravel()
@@ -87,14 +91,21 @@ def to_df(ds):
     df_psurf = df_psurf.ravel()
     df_prcp = np.array(prcp.transpose())
     df_prcp = df_prcp.ravel()
-    df_ws = df_uwind + df_vwind
+    df_uwind100 = np.array(u100.transpose())
+    df_uwind100 = df_uwind100.ravel()
+    df_vwind100 = np.array(v100.transpose())
+    df_vwind100 = df_vwind100.ravel()
+    df_ws = abs(df_uwind) + abs(df_vwind)
+    df_ws100 = abs(df_uwind100) + abs(df_vwind100)
     df_RH = np.array(611**(5423*((1/273)-(1/df_dewp)))/611**(5423*((1/273)-(1/df_tsurf)))*100)
 
-    df_all = np.array([df_lat, df_lon, df_psurf, df_sst, df_mslp, df_evap, df_prcp, df_tsurf, df_RH, df_ws])
+    df_all = np.array([df_lat, df_lon, df_psurf, df_sst, df_mslp, df_evap, df_prcp, df_tsurf, df_RH, df_ws, df_ws100])
     df_all = df_all.transpose()
     df_all = pd.DataFrame(df_all, columns = ['latitude', 'longitude', 'psurf', 'sst', 'mslp', 
-                                             'evaporation', 'prcp', 'tsurf', 'RH', 'ws'], dtype = 'float64')
+                                             'evaporation', 'prcp', 'tsurf', 'RH', 'ws', 'ws100'], dtype = 'float64')
     df_all = df_all.dropna()
+    df_all = df_all[(df_all['latitude'] > 15.75) | (df_all['longitude'] > -83.75)]
+    df_all = df_all[(df_all['latitude'] > 8.75) | (df_all['longitude'] > -77.25)]
     return df_all
 
 
@@ -108,7 +119,7 @@ def hurricane_plot(df_all, row_event, name_event, ds, i):
     i : paramètre pour permettre une boucle (sinon i = None)
     '''
     
-    Prediction = clf.predict(df_all.iloc[:,2:])
+    Prediction = rfc.predict(df_all.iloc[:,2:])
     df_all['prediction'] = Prediction
 
     sst = ds.variables['sst'][0, :, :]
@@ -141,7 +152,7 @@ def hurricane_plot(df_all, row_event, name_event, ds, i):
     cb.ax.set_title('SST [K]')
     ax.set_title("Hurricane Prediction of {} : {}".format(name_event, row_event.iloc[0,0]), size = 16)
     
-    plt.savefig('plots/katrina{:02d}.png'.format(i), dpi = 300)
+    plt.savefig('plots/katrina_{:02d}.png'.format(i), dpi = 100)
     plt.show()
 
   
@@ -158,50 +169,42 @@ def gif(png_dir, images, name):
             file_path = os.path.join(png_dir, file_name)
             images.append(imageio.imread(file_path))
             print(file_name)
-    imageio.mimsave('{}/{}.gif'.format(png_dir, name), images)
+    imageio.mimsave('{}/{}.gif'.format(png_dir, name), images, duration = 0.25)
 
-
-
-
-##### TEST DE PREDICTION DE L'OURAGAN KATRINA 2005  #####
-
+    
+    
+    ##### TEST DE PREDICTION DE L'OURAGAN KATRINA 2005  #####
+    
 df = pd.read_csv('hurricane_data.csv', sep = ',')
-
-# Application des conditions aux limites
-df.loc[df.sst < 300, 'nature'] = 0          
-df.loc[df.prcp < 0.0001, 'nature'] = 0
-df.loc[df.psurf > 100500, 'nature'] = 0
-
-# On peut identifier les ouragans et les non ouragans en créant des sous-df
-dfi = df[df['nature'] == 1]
-dfii = df[df['nature'] == 0]
-
-
-
+     
+    
 # On isole les évènements de Katrina
 df['time'] = pd.to_datetime(df['time'])
 df_kat = df[(df['name'] == 'KATRI2') & (df['time'].dt.year == 2005)]
+df_kat = df_kat.dropna()
 df_try = pd.merge(df, df_kat, how= 'outer', indicator = True)
 df_try = df.loc[df_try._merge == 'left_only']
-
-
+    
+ 
 # On entraine le modèle sur les données historiques (sans les évènements Katrina)
-X_train, X_test, y_train, y_test = train_test_split(df_try.iloc[:, 5:],
-                                                    df_try['nature'], random_state = 0)
+rfc = modelisation('hurricane_data', 298, 100500)   
 
-clf = RandomForestClassifier(n_estimators = 200, criterion = 'gini', random_state=1, class_weight = 'balanced')
-clf.fit(X_train, y_train)
-
-df_kat = df_kat[df_kat['time'].dt.day >= 25]
 
 for i in range(len(df_kat)):
-    a = pd.DataFrame(df_kat.iloc[i,:]).T
+    a = pd.DataFrame(df_kat.iloc[i,:]).T 
     hour = str(a.time.dt.hour.values[0]).zfill(2)
     day = str(a.time.dt.day.values[0]).zfill(2)
-    ds = API('2005', '08', day, hour, 'katrina.nc')
+    month = str(a.time.dt.month.values[0]).zfill(2)
+    year = str(a.time.dt.year.values[0]).zfill(2)
+    ds = API(year, month, day, hour, '2005.nc')
     df_all = to_df(ds)
-    hurricane_plot(df_all, a, 'KATRINA', ds, i)
-     
+    df_norm = (df_all - df_all.min())/(df_all.max() - df_all.min())
+    df_norm['latitude'] = df_all['latitude']
+    df_norm['longitude'] = df_all['longitude']
+    hurricane_plot(df_norm, a, 'KATRINA', ds, i)
+         
+    
 png_dir = 'C:/Users/baptc/Desktop/Master_SOAC/Master_1/S2/Projet_numerique/Data/plots'
 images = []
-gif(png_dir, images, 'katrina_movie')
+gif(png_dir, images, 'Katrina_movie')
+
